4e5ef4c51237339dec61b895b731c84d0b45a3fe
helobrouillette <heloise.brouillette@polymtl.ca>
2020-09-28 18:06:29 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -21,7 +21,9 @@ def anagrams(words: list = None) -> bool:
             liste1.append(i)
 
         for i in chaine2:
-            liste2.append()
+            liste2.append(i)
+
+        l
         print(words[0,])
 
     return False
