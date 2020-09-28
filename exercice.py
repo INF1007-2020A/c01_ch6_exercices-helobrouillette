8712de2e4253417126ce724497652b7aba1e2129
helobrouillette <heloise.brouillette@polymtl.ca>
2020-09-28 18:03:25 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,7 +14,8 @@ def anagrams(words: list = None) -> bool:
     if words is None:
         # TODO: Demander les mots ici
         chaine1 = input()
-        chaine2 = inp
+        chaine2 = input()
+        
 
         print(words[0,])
 
